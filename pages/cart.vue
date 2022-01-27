e89526a09fb0b3280ab860d0d97765d6b36126a8
lawrence-V <lawrencevaldez027@gmail.com>
2022-01-27 23:39:46 +0800

    Update cart.vue

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,15 +1,45 @@
 <template>
-  <div>
-      <h2>Cart</h2>
+<v-container>
+   <div v-for="item in cart" :key="item.id">
+    <v-data-table
+      dense
+      :headers="headers"
+      :items="item.item"
+      item-key="name"
+      class="elevation-1"
+    ></v-data-table>
   </div>
+</v-container>
+ 
 </template>
 
 <script>
-export default {
+import { mapState } from "vuex";
 
-}
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "Item",
+          align: "start",
+          sortable: false,
+          value: "name",
+        },
+        { text: "ADD ONS", value: "calories" },
+        { text: "Amount", value: "fat" },
+        { text: "Total Price", value: "carbs" },
+      
+      ],
+    };
+  },
+  computed: {
+    cart() {
+      return this.$store.state.cart;
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
